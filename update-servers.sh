2dc311ee6ac31538252547d96e53a25f047f32bb
RealiSimHQ <paddy.bot.00@gmail.com>
2026-03-19 16:51:01 -0400

    server cards: live car/track downloads from cm_content API, not hardcoded lists

--- update-servers.sh
+++ update-servers.sh
@@ -13,13 +13,26 @@ for port in range(9000, 9011):
             data = json.loads(r.stdout.split('___')[0] if '___' in r.stdout else r.stdout)
             if 'name' in data:
                 track = data.get('trackBase', data.get('track', '')).split('/')[-1].replace('-X10DD','')
+                # Extract cm_content car downloads
+                content_cars = {}
+                if 'content' in data and 'cars' in data['content']:
+                    content_cars = data['content']['cars']
+                # Extract cm_content track download
+                track_url = ''
+                if 'content' in data and 'track' in data['content']:
+                    track_url = data['content']['track'].get('url', '')
+                # Unique car list
+                car_models = list(dict.fromkeys(data.get('cars', [])))
                 results.append({
                     'port': port,
                     'online': True,
                     'clients': data.get('clients', 0),
                     'maxclients': data.get('maxclients', 0),
                     'name': data.get('name', ''),
-                    'track': track
+                    'track': track,
+                    'carModels': car_models,
+                    'carDownloads': {k: v.get('url','') for k,v in content_cars.items()},
+                    'trackDownload': track_url
                 })
                 continue
     except: pass
